2607c71bc0eb0bf439e3f348e7e8e166dbce90dd
A Certain TWOWer <30316430+miniTWOWHostingHelperWebApp@users.noreply.github.com>
2018-08-28 20:20:14 +0900

    Still not working well yet.
    .f, .e, .r, and so on, won't.

--- bringPxemBack.cpp
+++ bringPxemBack.cpp
@@ -4,32 +4,122 @@
 #include<string>
 using namespace std;
 
-/*
-class Pxem{
-	private:
-		string fname;
-		string fcont;
-		size_t itr;
-		stack tk;
-		int tmp;
-		
-		
-	public:
-		Pxem(string f, string c){
-			fname=extractor(f);
-			fcont=c;
-			
-			
-		}
-};
-*/
-
 string extractor(string s);
-int run(string fname, bool isFname);
+void run(string proc, bool d);
+void run(string proc);
+
+//This is a class designed for Pxem.
+class PxemStack{
+	private:
+		stack<int> stk;
+		//int temporal;	//Temporal Region
+		//bool temb;	//Whether the region is used or not
+	protected:
+		/*
+		PxemStack(stack<int> s){
+			temporal=0;
+			temb=false;
+			this.stk=s;
+		}
+		stack<int> lifeend(){
+			return this.stack;
+		}
+		*/
+	public:
+		/*
+		PxemStack(){
+			temporal=0;
+			temb=false;
+		}
+		*/
+		void push(int i){
+			stk.push(i);
+		}
+		int pop(){
+			int tmp = stk.top();
+			stk.pop();
+			return tmp;
+		}
+		bool empty(){
+			return stk.empty();
+		}
+		size_t size(){
+			return stk.size();
+		}
+		/*
+		void dott(){
+			//This is for .t operation.
+			temporal=pop();
+			temb=true;
+		}
+		void dotm(){
+			//This is for .m operation.
+			if(!temb)	return;
+			push(temporal);
+			temb=false;
+		}
+		*/
+		
+		/*
+		void dotp(){
+			while(!stack.empty()){
+				cout << char(stack.top());
+				stack.pop();
+			}
+		}
+		void doto(){
+			cout << char(stack.top());
+			stack.pop();
+		}
+		void dotn(){
+			cout << int(stack.top());
+			stack.pop();
+		}
+		void doti(){
+			char tmp;
+			cin >> tmp;
+			//TBH what does that mean "If you reach to EOF, it returns -1"?
+			if(cin::eof()){
+				tmp = -1;
+			}
+			stack.push(tmp);
+		}
+		void dot_(){
+			int tmp;
+			cin >> tmp;
+			stack.push(tmp);
+		}
+		void dotc(){
+			if(stack.empty())	return;
+			int tmp=stack.top();
+			stack.push(tmp);
+		}
+		void dots(){
+			if(stack.empty())	return;
+			stack.pop();
+		}
+		void dotv(){
+			size_t n=stack.size();
+			int tmp[n];
+			for(size_t i=0; !stack.empty(); i++){
+				tmp[i]=stack.top();
+				stack.pop();
+			}
+			for(size_t i=0; i<n; i++){
+				stack.push(tmp[i]);
+			}
+		}
+		void dotf(string f;){
+			while(!f.empty()){
+				stack.push(f.
+			}
+		}
+		*/
+};
 
 int main(int argc, char* argv[]){
 	try{
-		if(argc!=2){
+		if(argc!=2&&argc!=3){
 			throw "WRONG USAGE! You must use in this format!: ";
 		}
 	}catch(const char* e){
@@ -37,9 +127,7 @@ int main(int argc, char* argv[]){
 		return -1;
 	}
 	
-	//cout << argv[1] << endl;
-
-	ifstream fin(argv[1]);
+	static ifstream fin(argv[1]);
 
 	try{
 		if(!fin.is_open()){
@@ -50,14 +138,7 @@ int main(int argc, char* argv[]){
 		return -2;
 	}
 	
-	//cout << string(argv[1]) << endl;
-	string fname = argv[1];
-	
-	/*
-	cout << fname << endl;
-	cout << fname.rfind(".pxe") << ' ' << fname.rfind(".pxem") << endl;
-	cout << fname.length() << endl;
-	*/
+		string fname = argv[1];
 	
 	try{
 		if(fname.rfind(".pxe")==string::npos&&fname.rfind(".pxem")==string::npos){
@@ -72,7 +153,6 @@ int main(int argc, char* argv[]){
 	}
 	
 	//1.Run fname.
-	//2.If you get ".e" command, run contents.
 	//3.If you get ".d" command, stop running.
 	//4.If you get ".a" command before getting either ".w", ".x", ".y", ".z", that's error.
 	//5.If you didn't get ".a" command after getting those commands above, that's also an error.
@@ -83,7 +163,28 @@ int main(int argc, char* argv[]){
 	//cout << cont << endl;
 	*/
 	
-	return run(fname, true);
+	string proc=extractor(fname);
+	
+	//cout << proc << endl;
+	
+	try{
+		if(argc==3){
+			//cout << argv[2] << endl;
+			if(argv[2]=="-d"){
+				run(proc, true);
+			}
+			else{
+				run(proc);
+			}
+		}else{
+			run(proc);
+		}
+	}catch(const char* e){
+		cerr << e << endl;
+		return -4;
+	}
+	
+	return 0;
 }
 
 string extractor(string s){
@@ -99,78 +200,198 @@ string extractor(string s){
 	return s;
 }
 
-int run(string inp, bool isFname=false){
-	string proc=(isFname)?(extractor(inp)):inp;
-	try{
-		size_t ptr=0;
-		stack<int> stk;
-		bool sawDot=false;
-		while(ptr<proc.size()){
-			switch(proc[ptr]){
-				case '.':
-					switch(proc[++ptr]){
-						case 'p':
-							break;
-						case 'o':
-							break;
-						case 'n':
-							break;
-						case 'i':
-							break;
-						case '_':
-							break;
-						case 'c':
-							break;
-						case 's':
-							break;
-						case 'v':
-							break;
-						case 'f':
-							break;
-						case 'e':
-							break;
-						case 'r':
-							break;
-						case 'w':
-							break;
-						case 'x':
-							break;
-						case 'y':
-							break;
-						case 'z':
-							break;
-						case 'a':
-							break;
-						case 't':
-							break;
-						case 'm':
-							break;
-						case 'd':
-							break;
-						case '+':
-							break;
-						case '-':
-							break;
-						case '!':
-							break;
-						case '$':
-							break;
-						case '%':
-							break;
-						default:
-							break;
+void run(string proc, bool d){
+	PxemStack tk;	//stack
+	int reg;	//Temporal Region
+	bool reu=false;	//Whether reg is stored or not
+	
+	size_t ptr=0;	//Pointer
+	string str="";	//Temporal String until it ptr reaches to order
+	
+	int i1, i2;
+	char c1;
+	
+	//bool sawDot=false;
+	while(ptr<proc.size()){
+	
+		//From Below:Debug Part
+		//if(d){
+			cout << ptr << endl;
+			cout << str << endl;
+			
+			cout << proc << endl;
+			for(size_t i=0; i<ptr; i++){
+				cout << ' ';
+			}
+			cout << '^' << endl;
+		//}
+		//From Above:Debug Part
+		
+		if(proc[ptr]=='.'){
+			for(size_t i=str.size(); i>0; i--){
+				tk.push(str[i-1]);
+			}
+			str="";
+			switch(proc[++ptr]){
+				case 'p':
+					//Outputs the entire contents of tk as string.
+					while(!tk.empty()){
+						cout << (char)tk.pop();
 					}
-					++ptr;
+					cout << endl;
+					break;
+				case 'o':
+					//Outputs one popped valued as character.
+					if(!tk.empty()){
+						cout << (char)tk.pop();
+					}
+					break;
+				case 'n':
+					//Outputs one popped value as a numeral value.
+					if(!tk.empty()){
+						cout << (int)tk.pop();
+					}
+					break;
+				case 'i':
+					//Gets an input and push it as a character. If the data was EOF, -1 will be pushed.
+					cin >> c1;
+					tk.push((cin.eof())?-1:c1);
+					break;
+				case '_':
+					//Gets an input and push it as an integer.
+					
+					cin >> i1;
+					tk.push(i1);
+					break;
+				case 'c':
+					//Copies popped value and pushes two of them.
+					if(!tk.empty()){
+						c1=tk.pop();
+						tk.push(c1);
+						tk.push(c1);
+					}
+					break;
+				case 's':
+					//Just throws popped value away.
+					if(!tk.empty()){
+						tk.pop();
+					}
+					break;
+				case 'v':
+					//Reverses the content of stack. You should not do this for too many times.
+					int tmpn1=tk.size();
+					char tmp4[tmpn1];
+					for(size_t i=0; i<tmpn1; i++){
+						tmp4[i]=tk.pop();
+					}
+					for(size_t i=0; i<tmpn1; i++){
+						tk.push(tmp4[i]);
+					}
+					break;
+				case 'f':
+					//Pushes the content of file as string. You can use this command as many times as you'd like.
+					
+					break;
+				case 'e':
+					//Runs the content of file as Pxem code. This will be run at another process. The content of stack will be copied to new one. After it stops the content of new one will be stacked to old one.
+					break;
+				case 'r':
+					//Pops, then generate one random number that is 0 or bigger and smaller than popped value. Generated number will be pushed.
+					break;
+				case 'w':
+					//Pops the stack. If it is 0, skips to next to ".a".
+					if(!tk.empty()){
+						
+					}
+					break;
+				case 'x':
+					//Pops twice. If former-popped value ISN't smaller than latter one, skips to next to ".a".
+					if(tk.size()>=2){
+						
+					}
+					break;
+				case 'y':
+					//Pops twice. If former-popped value ISN'T bigger than latter one, skips to next to ".a".
+					if(tk.size()>=2){
+						
+					}
+					break;
+				case 'z':
+					//Pops twice. If they are equal, skips to next to ".a".
+					if(tk.size()>=2){
+						
+					}
+					break;
+				case 'a':
+					//Goes back to either .w, .x, .y, or .z. If no commands, that's an error.
+					break;
+				case 't':
+					//Stores popped-value to reu, the temporal region.
+					reg=tk.pop();
+					break;
+				case 'm':
+					//Pushes the value on the temporal region. If empty, don't.
+					if(reu){
+						tk.push(reg);
+					}
+					break;
+				case 'd':
+					//Ends the program.
+					return;
+					break;
+				case '+':
+					//Pops twice, then pushes its sum.
+					if(tk.size()>=2){
+						i1=tk.pop();
+						i2=tk.pop();
+						tk.push(i1+i2);
+					}
+					break;
+				case '-':
+					//Pops twice, then pushes its difference(but in positive integer).
+					if(tk.size()>=2){
+						i1=tk.pop();
+						i2=tk.pop();
+						tk.push((i1>i2)?(i1-i2):(i2-i1));
+					}
+					break;
+				case '!':
+					//Pops twice, then pushes its product.
+					if(tk.size()>=2){
+						i1=tk.pop();
+						i2=tk.pop();
+						tk.push(i1*i2);
+					}
+					break;
+				case '$':
+					//Pops twice, then pushes its .
+					if(tk.size()>=2){
+						i1=tk.pop();
+						i2=tk.pop();
+						tk.push((i1>i2)?(i1/i2):(i2/i1));
+					}
+					break;
+				case '%':
+					//Pops twice, then pushes its mod.
+					if(tk.size()>=2){
+						i1=tk.pop();
+						i2=tk.pop();
+						tk.push((i1>i2)?(i1%i2):(i2%i1));
+					}
 					break;
 				default:
-					stk.push(proc[ptr]);
-					++ptr;
+					//It was NOT a command: push those characters.
+					tk.push('.');
+					tk.push(proc[ptr]);
 					break;
 			}
+		}else{
+			str=str+proc[ptr];
 		}
-	}catch(const char* e){
-		cerr << e << endl;
-		return -4;
-	}	
-	return 0;
+		++ptr;
+	}
+}
+
+void run(string proc){
+	run(proc, false);
 }
